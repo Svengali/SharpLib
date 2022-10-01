82ab55c67266bb0a44378cc1cc5729589d253f15
Marc Hernandez <marcsh@gmail.com>
2022-10-01 02:42:44 -0700

    Add math fns

---
+++ math/fn.cs
@@ -0,0 +1,66 @@
+// Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
+// Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
+//
+// Copyright (c) 2010-2011 SharpDX - Alexandre Mutel
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace math
+{
+
+	static public class fn
+	{
+		static public float SmoothStepCos( float v )
+		{
+			var dV = (double)v;
+
+			var newV = 0.5 - 0.5 * Math.Cos( Math.PI * dV );
+
+			return (float)newV;
+
+		}
+
+		static public float SmoothStepSquare( float v )
+		{
+			var dV = (double)v;
+
+			var newV = dV * dV * (3.0 - 2.0 * dV);
+
+			return (float)newV;
+
+		}
+
+		static public float SmoothStepCube( float v )
+		{
+			var dV = (double)v;
+
+			var newV = dV * dV * dV * ( 6.0 * dV * dV - 15.0 * dV + 10.0 );
+
+			return (float)newV;
+
+		}
+	}
+
+}
